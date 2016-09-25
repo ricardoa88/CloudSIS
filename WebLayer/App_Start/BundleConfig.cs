b255e4110787253fcdce38f8d0008bece92257a9
ricardoa88 <ricardoa88@gmail.com>
2016-09-24 20:09:16 -0500

    Agregar signal r

--- WebLayer/App_Start/BundleConfig.cs
+++ WebLayer/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace WebLayer
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.signalR-2.2.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- WebLayer/Startup.cs
+++ WebLayer/Startup.cs
@@ -9,6 +9,7 @@ namespace WebLayer
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            app.MapSignalR();
         }
     }
 }
